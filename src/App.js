6626f8500d55aad1eccf15e38bc4b74d9d3b767e
Jamal Fakhriddinov <jamalfakhriddinov@Jamals-MacBook-Pro.local>
2021-07-19 01:48:59 +0800

    message

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import style from "./App.module.css"
 import AppComponent from './AppComponent'
 
 
-
-
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom' ;
+import { HashRouter as Router, Switch, Route } from 'react-router-dom' ;
 
 function App() {
   
@@ -13,12 +11,12 @@ function App() {
   return (
     <div className="App">
    
-   <Router basename={process.env.web}>
+   <Router basename='/'>
           <Switch>
             <Route path="/" exact  component ={AppComponent} />
            
         </Switch>
-    </Router>
+  </Router>
    
     </div>
   );
--- src/components/work.js
+++ src/components/work.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './work.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom' ;
+
 import {Link} from 'react-router-dom'
 
 function Work() {
